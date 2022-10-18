63615b7bb09e964d70f79fd95ae9dc779fb5fb3a
ekaterina-chern <113055918+ekaterina-chernykh@users.noreply.github.com>
2022-10-18 18:48:48 +0000

    sync

---
+++ main.cs
@@ -0,0 +1,21 @@
+// Created by: Ekaterina
+// Created on: Oct 2022
+//
+// This program calculates area of a triangle
+
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    // This function accepts user input
+    int heightOfTriangle;
+    int baseOfTriangle;
+    int area;      
+      
+    Console.WriteLine ("This program calculates the area of a triangle.");
+    Console.WriteLine(" ");
+
+    Console.Write("Enter the height (cm): ");
+    heightOfTriangle = Convert.ToInt32(Console.ReadLine())
+  }
+}
