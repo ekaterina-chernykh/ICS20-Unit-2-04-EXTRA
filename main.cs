ffe14a18a32449f928adbb93877db4a670967c13
ekaterina-chernykh <113055918+ekaterina-chernykh@users.noreply.github.com>
2022-10-19 12:06:05 -0400

    some whitespace fixes

--- main.cs
+++ main.cs
@@ -5,12 +5,12 @@
 
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    // This function accepts user input
-    int heightOfTriangle;
-    int baseOfTriangle;
-    int area;      
+class Program{
+   public static void Main(string[] args){
+     // This function accepts user input
+     int heightOfTriangle;
+     int baseOfTriangle;
+     int area;
       
     Console.WriteLine ("This program calculates the area of a triangle.");
     Console.WriteLine(" ");
